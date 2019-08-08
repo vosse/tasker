b4b064ee726ea1af8e7027f00382afb85f81a34a
voss-e <jovicevicdev@gmail.com>
2019-08-08 18:13:17 +0200

    Updates

---
+++ precache-manifest.3fda06116e3e2198a7d30d2bae44e70d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a79c35d83eb4d12587b1718773ab6ab",
+    "url": "/tasker/index.html"
+  },
+  {
+    "revision": "e29252f2d82c44ed4b31",
+    "url": "/tasker/static/css/main.e2bfb3cc.chunk.css"
+  },
+  {
+    "revision": "fa448fa0db502f0c9519",
+    "url": "/tasker/static/js/2.7a948928.chunk.js"
+  },
+  {
+    "revision": "e29252f2d82c44ed4b31",
+    "url": "/tasker/static/js/main.33347850.chunk.js"
+  },
+  {
+    "revision": "6d33110cf5b8c33a1266",
+    "url": "/tasker/static/js/runtime~main.240bf10a.js"
+  }
+]);
